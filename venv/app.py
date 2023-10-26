9be39f7469c5d63dbaebceaf2c329665813aad04
mifoak98 <148447434+mifoak98@users.noreply.github.com>
2023-10-26 13:26:29 +0200

    neo4j implementert
    Lagt til Neo4J

--- venv/app.py
+++ venv/app.py
@@ -1,2 +1,30 @@
 import json
 from flask import Flask, jsonify, request
+from neo4j import GraphDatabase
+
+app = Flask(__name__)
+
+class Neo4jDriver:
+    def __init__(self, uri, user, password):
+        self._driver = GraphDatabase.driver(uri, auth=(user, password))
+
+    def close(self):
+        self._driver.close()
+
+neo4j_driver = Neo4jDriver("neo4j+s://a0f6f9eb.databases.neo4j.io", "neo4j", "3hCqDbvBlbR_vFBCPgnp1JgwIj_NVyMUwOM6ybNVLsM")
+
+@app.route('/')
+def index():
+    with neo4j_driver._driver.session() as session:
+        result = session.run("MATCH (n) RETURN n LIMIT 25")
+        nodes = [record['n']['name'] for record in result]
+        
+    response_data = {
+        "data": nodes,
+        "message": "Hello INFO212"
+    }
+    
+    return jsonify(response_data)
+
+if __name__ == '__main__':
+    app.run()
